9ae5f19fb663b467d1570857ba0ed87a16408e82
hafsa-CH98 <chidmihafsa4@gmail.com>
2023-11-09 14:13:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,149 @@
+// for(let i = 1 ; 1 <=100 ; 1++ ){
+//     if(i % 2 === 0){
+//         console.log("je suis un nombre pair ==> "i")
+//     }else{
+//        console.log("je suis un nombre impair"==>"i")
+//     }
+// }
+
+
+
+// for(let i = 1 ; i <= 100; i++){
+//     if(i % 3 === 0 & i % 5 === 0){
+//         console.log("fizzBuzz!!")
+//     }else if( i % 5 === 0){
+//         console.log("bazz")
+//     }else if(i % 3 === 0){
+//         console.log("fizz")
+//     }else{
+//         console.log(i)
+//     }
+
+
+
+
+
+
+//console.log(myName[0])
+
+// for(let i=0;i<myName.lenght;i++){
+//     console.log(myName.length[i])
+// }
+
+
+//let perfectParentieses  = "(((((((((((((())()))(()()()))))))))))))("
+//console.log(myName[0])
+
+
+
+
+
+
+
+
+// for(let i=0;i<perfectParentieses;i++){
+//     console.log(perfectparentieses.length[i])
+// }
+
+
+
+
+
+
+
+
+// let myName ="gomycode"
+// console.log(myName[0])
+// console.log(myName[1])
+// console.log(myName[2])
+// console.log(myName[3])
+// console.log(myName[4])
+// console.log(myName[5])
+// console.log(myName[6])
+// console.log(myName[7])
+
+
+
+
+
+
+// let open = 0
+// let close = 0
+
+// let perfectParentieses  = "(((((((((((((())()))(()()()))))))))))))("
+
+// for(let i = 0 ;i < perfectParentieses.length  ; i++ ){
+//     if(perfectParentieses[i] === '('){
+//      open++
+//     }else if(perfectParentieses[i] === ')'){
+//      close= close + 1
+ 
+//   }
+ 
+//  }
+//  console.log('open ===>>>',open)
+//  console.log('close ==>>>',close)
+
+
+
+
+
+// function check(a, x) {
+//     // your code here
+//     for (let i = 0; i < a.length ; i++){
+//        if (a[i] === x){ 
+//         return true;
+//       }
+   
+//     }
+//     return false;
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+// function enough(cap, on, wait) {
+//     // your code here
+//     if(on + wait <=cap){
+//       return 0 
+//     }else if(on + wait > cap){
+//       return (wait -( cap - on))  
+//     }
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
